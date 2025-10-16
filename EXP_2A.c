e34eea5a9f5ea89634d4c0f0f1c2e062b4afdb7c
Ninja0415 <mayureshmene@eng.rizvi.edu.in>
2025-10-16 09:18:48 +0530

    Add files via upload

---
+++ EXP_2A.c
@@ -0,0 +1,22 @@
+/*
+Mayuresh Mene
+FE Computers
+DIV C
+UIN 251P016
+Roll no. 15
+*/
+#include<stdio.h>
+int main()
+{
+int a;
+printf("Enter a number=");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("Number is even");
+}
+else
+{
+printf("Number is odd");
+}
+}
